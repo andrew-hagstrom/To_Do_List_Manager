4e134f6e61d21828f29a092e8a6be4103f861ff9
Anthony_Quinones <Quinones.a009@gmail.com>
2023-10-06 16:24:53 -0500

    added input feature

--- test_todo_manager.py
+++ test_todo_manager.py
@@ -1,4 +1,7 @@
-from todolist import practice
+from todolist import user_input
 
-def test_helloworldfunction():
-    assert practice(1, 2)==3
+# def test_helloworldfunction():
+#     assert practice(1, 2)==3
+
+def test_user_input_function():
+    assert type(user_input("what's up?")) == str
--- todolist.py
+++ todolist.py
@@ -1,7 +1,9 @@
-def practice(num_one, num_two):
-    answer = num_one + num_two
+# def practice(num_one, num_two):
+#     answer = num_one + num_two
+#     return answer
+
+def user_input(question):
+    answer = input(question)
     return answer
 
-
-
-
+user_input("What's up?")
