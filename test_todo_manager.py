10f3e94f72e88e419f5cfffaac6f9670bdcb2153
Jkim1122 <jkimz1122@gmail.com>
2023-10-06 20:15:04 -0500

    this working?

--- test_todo_manager.py
+++ test_todo_manager.py
@@ -1,7 +1,12 @@
-from todolist import user_input
-
 # def test_helloworldfunction():
 #     assert practice(1, 2)==3
 
-def test_user_input_function():
-    assert type(user_input("what's up?")) == str
+from todolist import user_input
+from todolist import add_answer
+
+
+def test_user_input():
+    assert type(user_input("What's up?")) ==str
+
+def test_add_answer():
+    assert add_answer("Walk the dog") == ["Walk the dog"]
--- todolist.py
+++ todolist.py
@@ -1,9 +1,18 @@
 # def practice(num_one, num_two):
 #     answer = num_one + num_two
 #     return answer
+my_list = []
+answer = ""
 
-def user_input(question):
-    answer = input(question)
-    return answer
+while (True):
+    def user_input(question):
+        answer = input(question)
+        return(answer)
 
-user_input("What's up?")
+# user_input("What's is your task?")
+
+    def add_answer(task):
+        my_list.append(task)
+        print(my_list)
+
+    add_answer(user_input("What's is your task?"))
